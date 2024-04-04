b27052ed2c750a445b7185e0db36410204b4dee0
eliiz <eliiz@LAPTOP-3BVD5FHI>
2024-04-04 20:45:42 +0200

    Add project files.

---
+++ CLDV7111_2023_ST10129307_POE_1/Pages/AboutUs.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CLDV7111_2023_ST10129307_POE_1.Pages
+{
+    public class AboutUsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ CLDV7111_2023_ST10129307_POE_1/Pages/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CLDV7111_2023_ST10129307_POE_1.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
